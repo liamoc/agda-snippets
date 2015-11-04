7e84a2d8058796f8de90bda95a7c4bf9781c319d
Liam O'Connor <liamoc@cse.unsw.edu.au>
2015-11-05 04:12:30 +1100

    Build app on both dev and stable.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase, CPP #-}
 module Main where
 
 import Agda.Contrib.Snippets
@@ -8,17 +8,23 @@ import System.Exit
 import System.IO
 import Network.URI
 import Control.Applicative
-
+import Control.Monad.Trans.Except
 note :: b -> Maybe a -> Either b a 
 note b Nothing = Left b
 note _ (Just a) = Right a
 
+#if MIN_VERSION_Agda(2,4,3)
+getOptions = runExceptT . parseStandardOptions
+#else
+getOptions = return . parseStandardOptions
+#endif
+
 main :: IO ()
 main = getArgs >>= \case
    (fp:output:css:uri:agdaOpts) -> do
-     print (fp,css,uri,agdaOpts)
+     opts <- getOptions agdaOpts
      case liftA2 (,) (note "Malformed URI" $ parseURIReference uri)
-                     (parseStandardOptions agdaOpts)  of
+                     opts  of
        Right (uri', agdaOpts') -> writeFile output =<< renderAgdaSnippets agdaOpts' css uri' fp
        Left e -> hPutStrLn stderr e >> exitFailure
    _ -> do
